3d7607c3aa9e531720242e2666c731c9ea568b69
Prave1305 <prave1305@gmail.com>
2021-11-15 19:42:41 -0500

    js file added

---
+++ Login.js
@@ -0,0 +1,14 @@
+ function myFunction(){
+     var username=document.getElementById("username").Value;
+     var password=document.getElementById("password").Value;
+     if(username=="praveena"&& password=="sairam")
+     {
+     alert(Welcome);
+     return false;
+     }
+    else{
+        alert("sign up to proceed further");
+    }
+    }
+
+
