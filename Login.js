99dfe193a8ac097c054abed2d61a381031901e88
Prave1305 <prave1305@gmail.com>
2021-12-13 19:33:19 -0500

    move

--- Login.js
+++ Login.js
@@ -1,30 +1,19 @@
  function myFunction(){
-     var username=document.getElementById("username").value;
-     var password=document.getElementById("password").value;
-   
-    //  if(username==="praveena"&& password==="sairam")
-     {
-        localStorage.setItem(username="praveena",password="sairam");
-        // location.reload();
-        localStorage.getItem(username)
-        alert("Welcome");
-     }
-    // else{
-        alert("sign up to proceed further");
-    }
-    
-    localStorage.setItem("myUserName", username);
-     localStorage.setItem("myPassword", password);
-     localStorage.setItem("myUserData", {"username":username,"password" : password});
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+ 
+   localStorage.setItem("myUserName", username);
+    localStorage.setItem("myPassword", password);
+    // localStorage.setItem("myUserData", {"username":username,"password" : password});
 
-     if(username==="praveena"&& password==="sairam")
-     {
-        var myName = localStorage.getItem("myUserName");
-        alert("Welcome " + myName);
-     }
-    else{
-        alert("sign up to proceed further");
+    if(username==="praveena"&& password==="sairam")
+    {
+       var myName = localStorage.getItem("myUserName");
+       alert("Welcome " + myName);
     }
+   else{
+       alert("sign up to proceed further");
+   }
 
 }
 function clickOnfn(){
